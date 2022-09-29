5d84b3cdcf52ba101389229b1bdb6945d34a67bf
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2022-09-29 20:15:01 -0300

    tentando melhorar js

--- Projeto conversor de moedas/app.js
+++ Projeto conversor de moedas/app.js
@@ -12,3 +12,21 @@ document.getElementById('check-libra').addEventListener('click', function select
     let moedaConverter = document.querySelector('label#moedaConverter');
     moedaConverter.innerHTML = `Libra`;
 })
+function moeda() {
+
+    let input = document.getElementById('moedaConverter');
+    let inputMoeda = input.value;
+
+    if(inputMoeda == ''){
+        alert('oi');
+    }else{
+        let lista = inputMoeda.split('');
+        let tam = lista.length;
+        alert(tam);
+    
+        if (tam> 1 && tam == 3) {
+            inputMoeda[0] + ',';
+        }
+    }
+}
+moeda();
